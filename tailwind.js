a92205009726cdf9e1c32633b41061a77cbd7582
AlyssaLu <41v1rj@gmail.com>
2024-01-27 02:27:41 -0800

    tailwind configured, not tested

---
+++ tailwind.js
@@ -0,0 +1,6 @@
+// tailwind.js
+import { create } from 'tailwind-react-native-classnames';
+import styles from './tailwind.config.js'; // import your config
+
+const tw = create(styles);
+export default tw;
